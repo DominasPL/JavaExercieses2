34c3d06130dc9a1b69e262a622bb6d821a8c9c2e
DominasPL <dominik-stepuch@wp.pl>
2019-11-02 15:46:47 +0100

    Write a Java program to remove a specific element from an array.

--- src/arrays/JavaArrays.java
+++ src/arrays/JavaArrays.java
@@ -1,6 +1,10 @@
 package arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class JavaArrays {
 
@@ -20,6 +24,24 @@ public class JavaArrays {
         }
 
         return false;
+    }
+
+    //Write a Java program to remove a specific element from an array.
+    public int[] removeSpecificElement(int element) {
+
+        int indexToRemove = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == element) {
+                indexToRemove = i;
+            }
+        }
+
+        for (int i = indexToRemove; i < numbers.length - 1; i++) {
+            numbers[i] = numbers[i + 1];
+        }
+
+        return Arrays.copyOf(numbers, numbers.length - 1);
     }
 
 }
--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -8,8 +8,13 @@ public class Main {
 
         int[] numbers ={1, 2, 3, 4, 5, 6, 7, 8, 9};
         JavaArrays javaArrays = new JavaArrays(numbers);
-        boolean b = javaArrays.checkValueInArray(50);
-        System.out.println(b);
+//        boolean b = javaArrays.checkValueInArray(50);
+//        System.out.println(b);
+
+        for (int i : javaArrays.removeSpecificElement(2)) {
+            System.out.println(i);
+        }
+
 
     }
 
